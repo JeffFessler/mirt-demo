f68e6b3e7191ec9e12c4ad18e5b50b4766c7e800
fessler@umich.edu <fessler@umich.edu>
2019-06-24 15:53:06 -0400

    To 0.6.0

--- mri/mri-sim-2d+t.jl
+++ mri/mri-sim-2d+t.jl
@@ -1,12 +1,14 @@
 # 2D+T dynamic MRI simulation using GA radial sampling
-# reconstructed with temporal "TV" regularizer (corner-rounded)
-# 2019-06-13, Jeff Fessler
+# reconstructed with temporal "TV" regularizer (corner-rounded)  
+# 2019-06-13, Jeff Fessler  
+# 2019-06-23 update to use more realistic simulated sensitivity maps
 
 using MIRT
 using Plots: gui, plot, scatter
 using LinearAlgebra: norm, Diagonal
 using Random: seed!
 using LinearMaps
+jim(:abswarn, false); # suppress warnings about display of |complex| images
 
 # generate dynamic image sequence
 if !@isdefined(xtrue)
@@ -23,14 +25,17 @@ if !@isdefined(xtrue)
 		tmp[2,4] = 15 + 5 * sin(2*pi*it/nt) # mouth open/close
 		ellpars[:,:,it] = tmp
 		xtrue[:,:,it] = ellipse_im(ig, ellpars[:,:,it], oversample=4)
-		jim(ig.x(), ig.y(), xtrue[:,:,it], title="frame $it")
+		jim(ig.x, ig.y, xtrue[:,:,it], title="frame $it")
 		gui()
 	end
 end
+jim(xtrue, yflip=ig.dy < 0)
 
 # plot one time course to see temporal change
-# plot(1:nt, abs.(xtrue[30,14,:]))
+ix,iy = 30,14
+plot(1:nt, abs.(xtrue[ix,iy,:]), label="ix=$ix, iy=$iy", xlabel="frame")
 
+# +
 # k-space sampling and data
 if !@isdefined(kspace)
 	@show accelerate = 3
@@ -38,20 +43,26 @@ if !@isdefined(kspace)
 	Nro = maximum(N)
 	Nspoke = nspf * nt
 	kspace = ir_mri_kspace_ga_radial(Nro = Nro, Nspoke = Nspoke)
-	kspace ./= ig.fov
+	kspace[:,:,1] ./= ig.fovs[1]
+	kspace[:,:,2] ./= ig.fovs[2]
 	kspace = reshape(kspace, Nro, nspf, nt, 2)
 end
 
-if false # plot sampling
+if true # plot sampling
 	ps = Array{Any}(undef, nt)
 	for it=1:nt
 		ps[it] = scatter(kspace[:,:,it,1], kspace[:,:,it,2], aspect_ratio=1, label="")
 		plot(ps[it])
 		gui()
 	end
-#	plot(ps...)
+	plot(ps..., layout=(2,4))
 end
+# -
 
+# make sensitivity maps
+ncoil = 2
+(smap,_) = ir_mri_sensemap_sim(dims=N, ncoil=ncoil, orbit_start=90)
+jim(smap, "ncoil=$ncoil sensitivity maps")
 
 # make system matrix for dynamic non-Cartesian parallel MRI
 if !@isdefined(A)
@@ -64,15 +75,17 @@ if !@isdefined(A)
 
 	# block diagonal system matrix, with one NUFFT per frame
 	# todo: more realistic coil sensitivity
-	ncoil = 2
-	S = [Diagonal(ic*ones(Float32, N)[:]) for ic=1:ncoil] # silly sensitivity maps
+	ncoil = size(smap,3)
+#	S = [Diagonal(ic*ones(Float32, N)[:]) for ic=1:ncoil] # silly sensitivity maps
+	S = [Diagonal(ic*ones(Float32, N)[:]) for ic=1:ncoil] # simulated sensitivity maps
 	Slm = block_lm(S, how=:col) # [S1; ... ; Sncoil]
 	mykron = A1 -> block_lm([A1], how=:kron, Mkron=ncoil) # one NUFFT per coil
 
 	# output is essentially [nt Ncoil nspf Nro] (which is possibly unusual)
 	# input is [N... nt]
-	A = block_lm([mykron(s.A)*Slm for s in ns], how=:diag)
+	A = block_lm([mykron(s.A)*Slm for s in ns], how=:diag);
 end
+size(A)
 
 
 # simulate k-space data via an inverse crime
@@ -91,15 +104,14 @@ end
 
 # initial image via zero-fill and scaling
 if !@isdefined(x0)
-	# todo: need density compensation, perhaps via
+	# todo: should use density compensation, perhaps via
 	# https://github.com/JuliaGeometry/VoronoiDelaunay.jl
 	x0 = reshape(A' * y, N..., nt) # zero-filled recon
 
 	tmp = A * x0[:]
 	x0 = (tmp'y / norm(tmp)^2) * x0 # scale sensibly
-	jim(x0, yflip=ig.dy < 0)
-	gui()
 end
+jim(x0, "initial image", yflip=ig.dy < 0)
 
 
 """
@@ -139,18 +151,16 @@ end
 # temporal finite differences
 if !@isdefined(Dt)
 	N3i = (N..., nt) # (nx ny nt)
-    N3o = (N..., nt-1) # (nx ny nt-1)
-    Dt = make_Dt(N3i, T=eltype(A))
+	N3o = (N..., nt-1) # (nx ny nt-1)
+	Dt = make_Dt(N3i, T=eltype(A))
+end
+
 	tmp = reshape(Dt * xtrue[:], N3o)
 #	jim(tmp), gui()
 	tmp = reshape(Dt' * tmp[:], N3i)
-	jim(tmp), gui()
-end
+	jim(tmp)
 
-if false
-end
-
-# prepare to run nonlinear CG
+# run nonlinear CG
 if !@isdefined(xh)
 	niter = 90
 	delta = Float32(0.1) # small relative to temporal differences
@@ -169,18 +179,16 @@ if !@isdefined(xh)
 end
 
 # show results
-if true
 	plot(layout=(2,2),
 		jim(xtrue, "xtrue", yflip=ig.dy < 0),
 		jim(xh, "recon", yflip=ig.dy < 0),
 		jim(xh-xtrue, "error", yflip=ig.dy < 0),
 		scatter(0:niter, log.(costs), label="cost", xlabel="iteration"),
 	)
-	gui()
-end
 
 
-if false
+
+#=
 
 """
 `stackpick(x::AbstractArray{<:Any}, i::Int)`
@@ -189,7 +197,7 @@ return `x[:,...,:,i]`
 function stackpick(x::AbstractArray{<:Any}, i::Int)
 	xdim = size(x)
 	dim1 = xdim[1:end-1]
-	return reshape((@view reshape(x, prod(dim1), xdim[end])[:,i]), dim1) 
+	return reshape((@view reshape(x, prod(dim1), xdim[end])[:,i]), dim1)
 end
 
 	forw = x -> begin
@@ -202,3 +210,5 @@ end
 	tmp = forw(xtrue)
 	@show size(tmp)
 end
+
+=#
